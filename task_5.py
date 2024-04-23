c3e0745aad31609d8eb826cf7ad90c208dd11a92
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-23 15:59:34 +0500

    задание 5

--- task_5.py
+++ task_5.py
@@ -7,23 +7,18 @@ class Employee:
 
 
 class Manager(Employee):
-    def __init__(self, salary=1500):
-        self.salary = salary
 
     def get_paid(self):
         return f"Зарплата менеджера = {self.salary}$"
 
 
 class Worker(Employee):
-    def __init__(self, salary=500):
-        self.salary = salary
-
     def get_paid(self):
         return f"Зарплата работника = {self.salary}$"
     
 
-manager_1 = Manager()
-worker_1 = Worker()
+manager_1 = Manager(1500)
+worker_1 = Worker(500)
 
 print(manager_1.get_paid())
 print(worker_1.get_paid())
