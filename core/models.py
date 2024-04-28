e78eed88eca1be7f0fdfcc88d37a085c7c34f037
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-28 23:55:22 +0500

    1) Создание приложения core 2) Добавление двух моделей Animal и Insects 3) Создание двух предстовлений одно из которых выводит текущую дату и время

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField('Название', max_length=100)
+
+    class Meta:
+        verbose_name = 'Животное'
+        verbose_name_plural = 'Животные'
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Insects(models.Model):
+    name = models.CharField('Название', max_length=100)
+
+    class Meta:
+        verbose_name = 'Насекомое'
+        verbose_name_plural = 'Насекомые'
+
+    def __str__(self) -> str:
+        return self.name    
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello!')
+
+def time(request):
+    return HttpResponse(f'<h1>Текущая дата и время</h1><p>{datetime.now()}</p>')
