7a0d14865ec2d877e2ffa18c9919993417139b68
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-05-08 22:08:59 +0500

    Django Templates

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Animal)
-admin.site.register(models.Birds)
-admin.site.register(models.Staff)
+# admin.site.register(models.Animal)
+# admin.site.register(models.Birds)
+# admin.site.register(models.Staff)
+admin.site.register(models.News)
