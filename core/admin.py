a93f122f082dbdfd4ea78fe4ed1a1157903a5ee7
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-05-20 19:44:33 +0500

    Django. Class-Based Views

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . import models
 
 
-# admin.site.register(models.Animal)
-# admin.site.register(models.Birds)
-# admin.site.register(models.Staff)
+admin.site.register(models.Animal)
+admin.site.register(models.Birds)
+admin.site.register(models.Staff)
 admin.site.register(models.News)
 admin.site.register(models.Category)
