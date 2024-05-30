8f3ee0bb878477a02881068010265413a1cf82c5
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-05-30 18:34:28 +0500

    clear branch

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-admin.site.register(models.Animal)
-admin.site.register(models.Birds)
-admin.site.register(models.Staff)
-admin.site.register(models.News)
-admin.site.register(models.Category)
